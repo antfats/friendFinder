393c5c88aa2b902201ccb281d0ee596673987b9a
bob <antfats@hotmail.com>
2019-04-22 11:47:09 -0700

    finished the server, almost

---
+++ server.js
@@ -0,0 +1,29 @@
+var express = require("express");
+var path = require("path");
+//setting up servers 
+var app = express();
+var port = 8080;
+
+//filters between the server and the application
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static(__dirname + '/'));
+
+//Store data here
+var person = [{
+    questionOne: 0,
+    questionTwo:0,
+    questionThree:0,
+    questionFour:0,
+    questionFive:0,
+    questionSix:0,
+    questionSeven:0,
+    questionEight:0,
+    questionNine:0,
+    questionTen0:0
+},]
+
+app.get("/",function(req,res){
+    res.sendFile(path.join(__dirname, "#"));
+});
+
