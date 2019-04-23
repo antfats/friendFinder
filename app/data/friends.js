2b2432c4a6441730ae3ff9fc38839d260e8cb20e
bob <antfats@hotmail.com>
2019-04-23 12:56:39 -0700

    friend.js complete

---
+++ app/data/friends.js
@@ -0,0 +1,14 @@
+//Store data here
+var person = [{
+    questionOne: 0,
+    questionTwo: 0,
+    questionThree: 0,
+    questionFour: 0,
+    questionFive: 0,
+    questionSix: 0,
+    questionSeven: 0,
+    questionEight: 0,
+    questionNine: 0,
+    questionTen0: 0
+},]
+module.exports = person;
--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -1,22 +1,16 @@
-var express = require("express");
-var path = require("path");
-//setting up servers 
-var app = express();
-var port = 8080;
-
-//filters between the server and the application
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
-app.use(express.static(__dirname + '/'));
 
 
-//Routes
-app.get("/", function (req, res) {
-    res.sendFile(path.join(__dirname, "../public/home.html"));
-});
+module.exports = function (app) {
+    //Routes
+    app.get("/", function (req, res) {
+        res.sendFile(path.join(__dirname, "../public/home.html"));
+    });
 
-app.get("/survey", function (req, res) {
-    res.sendFile(path.join(__dirname, "../public/survay.html"))
-})
-//------------------------------------------------------------
-
+    app.get("/survey", function (req, res) {
+        res.sendFile(path.join(__dirname, "../public/survay.html"))
+    })
+    //------------------------------------------------------------
+    app.get("*", function (req, res) {
+        res.sendFile(path.join(__dirname, "../public/home.html"));
+    });
+}
